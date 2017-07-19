ad1371bb7af9cf919c714276bdae402fc73c4350
VolodymyrTrykoz <mr.mastachok@gmail.com>
2017-07-19 10:00:06 +0300

    commit

---
+++ js/js.js
@@ -0,0 +1,27 @@
+ $(document).ready(function(){
+	 $('.pict_slider').slick({
+	  infinite: true,
+	  slidesToShow: 19,
+  	  slidesToScroll: 10,
+	});//end slick 
+	$(function (){
+		$('.menuToggle').on('click', function(){
+
+			$('.menu').slideToggle(300, function(){
+
+				if($(this).css('display') === 'none') {
+						$(this).removeAttr('style');
+				}; // end this
+			}); // end slideToggle
+		}); // end menuToggle
+	}); // end function
+
+	$('.img').hover(function(){
+		$(this).css('opacity','0.5')
+	}, function (){
+		$(this).css('opacity','1')
+	}); // end hover
+
+});//end ready
+
+
